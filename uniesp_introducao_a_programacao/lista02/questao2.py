f697a3ed8daf8de1f8d694a1f31615050cb017bb
analiciaguedes <112355811+analiciaguedes@users.noreply.github.com>
2022-09-14 19:58:01 -0300

    Delete uniesp_introducao_a_programacao/lista02 directory

--- uniesp_introducao_a_programacao/lista02/questao2.py
+++
@@ -1,6 +0,0 @@
-for num in range(1, 11): 
-    print(f"tabuada do {num}")
-
-    for num2 in range(1, 11):
-        resultado = num * num2 
-        print(f'{num} x {num2} = {resultado}')
--- uniesp_introducao_a_programacao/lista02/questao3.py
+++
@@ -1,5 +0,0 @@
-lista_amigos = ['Analicia', 'luis', 'galvao bueno'] 
-for nome in lista_amigos: 
-    print(f'{nome}, obrigado por vir a aula!')
-
-    
